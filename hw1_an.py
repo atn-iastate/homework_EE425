89eb4b6de6fc4d0d40e7c49f5169202638d39757
An <atn@iastate.edu>
2020-01-28 19:16:01 -0600

    working with real data

--- hw1_an.py
+++ hw1_an.py
@@ -9,7 +9,20 @@ for t in raw:
 
 airfoil = np.array(airfoil)
 x = airfoil[:, 0:5]
-y = airfoil[:, 5]
-print(x)
-print(y)
+intercept = np.ones(len(airfoil)).reshape(len(airfoil), 1)
+x = np.hstack((x, intercept))
+y = airfoil[:, 5].reshape(len(airfoil), 1)
 
+# regression
+train_w = np.asarray([0, 0, 0, 0, 0, 0]).reshape(6, 1)
+max_iter = 10000
+learning_rate = 1e-15
+
+for i in range(max_iter):
+    gradient = np.dot(np.transpose(y - np.dot(x, train_w)), x)
+    train_w = train_w - learning_rate * np.transpose(gradient)
+
+solution = np.dot(np.linalg.inv(np.dot(np.transpose(x), x)), np.dot(np.transpose(x), y))
+print(train_w)
+print(solution)
+print(np.linalg.norm(train_w - solution)/np.linalg.norm(solution))
---
+++ test.py
@@ -0,0 +1,3 @@
+import numpy as np
+a = 5 * np.asarray([1,2,3,4,5])
+print(a)
